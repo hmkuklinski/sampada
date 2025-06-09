bc3dd530fa3983528c27584a9a1a9abc496bc8ff
hmkuklinski <hmkuklinski@gmail.com>
2025-06-09 03:26:42 -0400

    media landing layout. general basic for about.
    wait for sampada's feedback for formatting of aboutlanding

---
+++ portfolio/src/components/AboutLanding.jsx
@@ -0,0 +1,27 @@
+import Socials from './Socials';
+export default function AboutLanding(){
+    return (
+        <div className="about-container">
+            <div className="about-left">
+               <div className="about-photo">
+                    <img src="/assets/headshots/sampada2.jpg" alt="headshot" />
+                </div> 
+                
+            </div>
+            
+            <div className="about-information">
+                <div className="about-heading">
+                    <h2>Sampada Acharya</h2>
+                </div>
+                <div className="about-subheading">
+                    <h4><i>Subheading Here</i> </h4>
+                </div>
+                <div className="about-text">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic, ut. Debitis nesciunt voluptatum eveniet deleniti incidunt aut quod nam pariatur maiores assumenda, numquam beatae repellat maxime dolores voluptates aspernatur quidem.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ea doloribus neque aspernatur earum dolore praesentium facilis? Numquam beatae id ratione earum at, excepturi assumenda quibusdam obcaecati pariatur optio explicabo.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ea doloribus neque aspernatur earum dolore praesentium facilis? Numquam beatae id ratione earum at, excepturi assumenda quibusdam obcaecati pariatur optio explicabo.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
--- portfolio/src/components/PageLanding.jsx
+++ portfolio/src/components/PageLanding.jsx
@@ -1,14 +1,39 @@
-export default function PageLanding(){
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
+
+export default function PageLanding() {
+    const typedRef = useRef(null);
+
+    useEffect(() => {
+        const typedStrings = ["Sampada Acharya", "An Amazing Person", "A Leader", "An Innovative Researcher"];
+        const typed = new Typed(typedRef.current, {
+            strings: typedStrings,
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
     return (
         <div className="page-landing">
-            <div className="landing-headshot">
-                <img src="/assets/sampada2.jpg" alt="sampada-headshot"/>
-            </div>
             <div className="landing-information">
-                <div className="landing-text">
-
+                <div className="landing-header">
+                    <h1>
+                        I'm <span ref={typedRef}></span>
+                    </h1>
                 </div>
-                <div className="landing-keywords"></div>
+                <div className="landing-text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quasi aliquid fugit minima impedit doloremque amet repudiandae perferendis tempora, recusandae iure delectus neque voluptate consequatur, odio architecto omnis, fuga obcaecati?
+                </div>
+                <div className="landing-keywords">
+                    <b>Keywords: </b> keyword, keyword, keyword, keyword
+                </div>
+                
             </div>
         </div>
     );
