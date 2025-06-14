5015ae4ccd5be7badd59d1480aa4d63bf877c7bf
hmkuklinski <hmkuklinski@gmail.com>
2025-06-14 08:02:13 -0400

    projects page. layout for project cards, functional search/reset options

--- portfolio/src/components/AboutLanding.jsx
+++ portfolio/src/components/AboutLanding.jsx
@@ -1,21 +1,23 @@
 import Socials from './Socials';
+import { sampadaName, sampadaPhotoPaths } from '../sampadaInfo';
 export default function AboutLanding(){
     return (
         <div className="about-container">
             <div className="about-left">
                <div className="about-photo">
-                    <img src="/assets/headshots/sampada2.jpg" alt="headshot" />
+                    {/* sampadaPhotoPaths index 0 - headshot information */}
+                    <img src={sampadaPhotoPaths[0].info} alt={sampadaPhotoPaths[0].id} />
                 </div> 
-                
             </div>
             
             <div className="about-information">
                 <div className="about-heading">
-                    <h2>Sampada Acharya</h2>
+                    <h2>{sampadaName}</h2>
                 </div>
                 <div className="about-subheading">
                     <h4><i>Subheading Here</i> </h4>
                 </div>
+                <Socials />
                 <div className="about-text">
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic, ut. Debitis nesciunt voluptatum eveniet deleniti incidunt aut quod nam pariatur maiores assumenda, numquam beatae repellat maxime dolores voluptates aspernatur quidem.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ea doloribus neque aspernatur earum dolore praesentium facilis? Numquam beatae id ratione earum at, excepturi assumenda quibusdam obcaecati pariatur optio explicabo.</p>
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,10 +1,154 @@
 import Layout from "./Layout";
+import Project from "./Project";
+import { sampadaProjects, sampadaUpcoming, allProjects } from "../sampadaInfo";
+import { useState, useEffect} from "react";
 export default function Projects(){
+    const [keywords, setKeywords] = useState("");
+    const [showError, setShowError] = useState(false);
+    const [isFiltering, setIsFiltering] = useState(false);
+    // const [showSearch, setShowSearch]= useState(false);
+    const [showX, setShowX] = useState(false);
+
+    //check to see if keywords input is deleted:
+    useEffect(() => {
+        if (keywords === "") {
+            setIsFiltering(false); //show default projects selection
+            setShowError(false);
+        }
+        else{
+            setShowX(true);
+        }
+    }, [keywords]);
+   
+    //check to see if the keyword enters matches tag or info
+    const validateSearch = () =>{
+        setIsFiltering(true);
+        const filtered = allProjects.filter(project =>
+            project.tags.toLowerCase().includes(keywords.toLowerCase()) ||
+            project.info.toLowerCase().includes(keywords.toLowerCase())
+        );
+        //show error if there are no matching terms:
+        if (keywords && filtered.length === 0) {
+            setShowError(true);
+        } else {
+            setShowError(false);
+        }
+    }
+
+    //reset all the states!
+    const handleReset = () => {
+        setKeywords("");
+        setIsFiltering(false);
+        setShowError(false);
+        setShowX(false);
+    };
+
+    //if keyword is in tags or in description, add to filtered projects
+    //if not, leave filteredProjects as an empty array
+    const filteredProjects = keywords ? allProjects.filter(project =>
+            project.tags.toLowerCase().includes(keywords.toLowerCase()) ||
+            project.info.toLowerCase().includes(keywords.toLowerCase())
+        ): [];
+
+
+    //shows all projects, divided into completed and in progress, with headers!
+    let defaultPageInfo = (
+        <div className="page-container">
+            <div className="current-projects">
+                <div className="header-wrapper">
+                    <h3 className="header-text">Completed Projects</h3>
+                    <div className="underline">
+                        <span className="circle"></span>
+                    </div>
+                </div>
+                <div className="current-content">
+                    {sampadaProjects.map((project)=>(
+                        <Project photo={project.photo} title={project.id} description={project.info} link={project.link} subtitle={project.subtitle} tags={project.tags} status={project.status}/>
+                    ))}
+                </div>
+            </div>
+            <div className="current-projects">
+                <div className="header-wrapper">
+                    <h3 className="header-text">Upcoming Projects</h3>
+                    <div className="underline">
+                        <span className="circle"></span>
+                    </div>
+                </div>
+                <div className="current-content">
+                    {sampadaUpcoming.map((project)=>(
+                        <Project photo={project.photo} title={project.id} description={project.info} link={project.link} subtitle={project.subtitle} tags={project.tags} status={project.status}/>
+                    ))}
+                </div>
+            </div>
+            
+        </div>
+    );
+
+    //shows only the filtered results based on keywords:
+    let filteredResults = (
+        <div className="page-container">
+             <div className="current-content">
+                {filteredProjects.map((project)=>(
+                    <Project photo={project.photo} title={project.id} description={project.info} link={project.link} subtitle={project.subtitle} tags={project.tags} status={project.status}/>
+                ))}
+            </div>
+        </div>
+    );
+
+    //the error message that will display if no keywords match:
+    let errorMessage = (
+        <div className="error-container">
+            <div className="error-header">
+                <h2>No Matches Found</h2>
+            </div>
+            <div className="error-text">
+                <p>
+                    There are no projects that match your search entry. 
+                    Please try again.
+                </p>
+            </div>
+            <div className="error-button" onClick={handleReset}>
+                X
+            </div>
+        </div>
+    );
+
+    //has the search input, search button, and reset button for project search:
+    let searchOption = (
+        <div className="search-option">
+            <div className="search-input">
+                <input type="text" placeholder="Search projects..." value={keywords} onChange={(e) => setKeywords(e.target.value)} 
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        validateSearch();
+                        }
+                }}/>
+            </div>
+            {showX && <div className="search-x" onClick={handleReset}>X</div>}
+            
+            <div className="search-button">
+                <button onClick={validateSearch}><ion-icon name="search-outline"></ion-icon></button>
+            </div>
+        </div>
+    );
+
+    // const showSearchBtn = (
+    //     <div className="show-search" onClick={()=>{setShowSearch(true)}}>
+    //         Search Projects <ion-icon name="search-outline"></ion-icon>
+    //     </div>
+    // );
+
     return (
         <Layout>
-            <div className="page-container">
-                <h2>Projects</h2>
-            </div>
+            {/* {!showSearch && showSearchBtn} */}
+            {/* {showSearch && searchOption} */}
+            {searchOption}
+
+            {/* if the error message needs to be displayed, render errorMessage jsx */}
+            {showError && errorMessage}
+
+            {/* if there are filtering options and do not need error message --> show filtered results. otherwise, show default page info. */}
+            {isFiltering && !showError ? filteredResults : defaultPageInfo}
         </Layout>
     );
 }
