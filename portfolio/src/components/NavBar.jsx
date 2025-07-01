9e1e06d6aa84b71646c80f3e123eaec3debae51e
hmkuklinski <hmkuklinski@gmail.com>
2025-07-01 17:17:58 -0400

    mobile formatting for all existing sections
    edits to container heights and formatting

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,19 +1,70 @@
 import { NavLink } from "react-router-dom";
+import {useState, useEffect} from "react";
 export default function NavBar(){
+    const [isMobile, setIsMobile] = useState(false);
+    const [showHamburger, setShowHamburger]= useState(false);
+    
+    //check if the user is on mobile:
+    useEffect(() => {
+    function handleResize() {
+        setIsMobile(window.innerWidth < 768);
+    }
+    handleResize(); // check on mount
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+    }, []);
+    
+    //toggles the menu options display:
+    const toggleHamburger=()=>{
+        setShowHamburger(prev =>!prev);
+    }
+
+    //holds the ul for the navbar (with li elements)
+    //note: added onclick to close the hamburger options if a link is clicked (so user can see whole page)
+    const navbarUL = (
+        <ul>
+            <li onClick={() => setShowHamburger(false)}><NavLink to="/">Home</NavLink></li>
+            <li onClick={() => setShowHamburger(false)}><NavLink to="/projects" >Projects</NavLink></li>
+            <li onClick={() => setShowHamburger(false)}><NavLink to="/publications" >Publications</NavLink></li>
+            <li onClick={() => setShowHamburger(false)}><NavLink to="/thesis" >Thesis</NavLink></li>
+            <li onClick={() => setShowHamburger(false)}><NavLink to="/cv" >CV</NavLink></li>
+            <li onClick={() => setShowHamburger(false)}><NavLink to="/awards" >Awards</NavLink></li>
+            <li onClick={() => setShowHamburger(false)}><NavLink to="/community" >Community</NavLink></li>
+        </ul>
+    );
+
+    //hamburger menu option for mobile:
+    const mobileBtn = (
+        <div className="nav-button">
+            <button className="hamburger" onClick={toggleHamburger}>☰</button>
+        </div>
+    );
+
+    //holds the name/home link:
+    const navbarName = (
+        <div className="navbar-name">
+            <NavLink to="/"><h2>Sampada Acharya</h2></NavLink>
+        </div>
+    );
+
+    //mobile display:
+    if (isMobile){
+        return (
+        <nav className="navbar-container">
+            {navbarName}
+            {mobileBtn}
+            <div className="nav-links" style={{display: showHamburger? "block": "none"}}>
+            {navbarUL}
+            </div>
+        </nav>
+        )
+    }
+    //desktop or tablet view:
     return (
         <div className="navbar-container">
-            <div className="navbar-name">
-                <NavLink to="/"><h2>Sampada Acharya</h2></NavLink>
-            </div>
+            {navbarName}
             <div className="navbar-links">
-                <ul>
-                    <li><NavLink to="/projects" >Projects</NavLink></li>
-                    <li><NavLink to="/publications" >Publications</NavLink></li>
-                    <li><NavLink to="/thesis" >Thesis</NavLink></li>
-                    <li><NavLink to="/cv" >CV</NavLink></li>
-                    <li><NavLink to="/awards" >Awards</NavLink></li>
-                    <li><NavLink to="/community" >Community</NavLink></li>
-                </ul>
+                {navbarUL}
             </div>
         </div>
     );
