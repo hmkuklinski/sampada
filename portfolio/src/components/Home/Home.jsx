f5ce0b0bae244a99b8eb9ca8f88c063e5edb727d
unknown <hmkuklinski@gmail.com>
2025-10-26 03:51:52 -0400

    bug fixes

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,13 +1,13 @@
 import Layout from "../Layout";
 import AboutLanding from "./AboutLanding";
-import Timeline from "./Timeline";
-import MyHeader from "../MyHeader";
+// import Timeline from "./Timeline";
+// import MyHeader from "../MyHeader";
 export default function Home(){
     return (
         <Layout>
             <div className="page-container">
                 <AboutLanding />
-                <MyHeader title="Background" id="background-header" />
+                {/* <MyHeader title="Background" id="background-header" /> */}
                 {/* <Timeline /> */}
             </div>
         </Layout>
