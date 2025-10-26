bcdedf14bfbe92715cea2f9043ada54af9d05f92
unknown <hmkuklinski@gmail.com>
2025-10-26 05:34:47 -0400

    have to do downloads for project pdfs now...

--- portfolio/src/components/Research/Project.jsx
+++ portfolio/src/components/Research/Project.jsx
@@ -44,10 +44,10 @@ export default function Project({photo, link, title, subtitle, description, vidL
                     </div>
                     <div className="project-link">
                         <div className="project-link-div">
-                            <a href={link} target="_blank" rel="noopener noreferrer">PDF<ion-icon name="chevron-forward-outline"></ion-icon></a>
+                            <a href={link} download rel="noopener noreferrer">PDF<ion-icon name="chevron-forward-outline"></ion-icon></a>
                         </div>
                         <div className="project-link-div">
-                            {articleLink && <a href={articleLink} target="_blank" rel="noopener noreferrer">Article<ion-icon name="chevron-forward-outline"></ion-icon></a>}
+                            {articleLink && <a href={articleLink} download rel="noopener noreferrer">Article<ion-icon name="chevron-forward-outline"></ion-icon></a>}
                         </div>
                         <div className="project-link-div">
                             {vidLink && (
