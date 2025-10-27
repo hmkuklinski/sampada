188c7bb1de5c53c49e927307978d7c7acc1b454a
unknown <hmkuklinski@gmail.com>
2025-10-26 23:17:20 -0400

    timeline edits

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,14 +1,14 @@
 import Layout from "../Layout";
 import AboutLanding from "./AboutLanding";
-// import Timeline from "./Timeline";
-// import MyHeader from "../MyHeader";
+import Timeline from "./Timeline";
+import MyHeader from "../MyHeader";
 export default function Home(){
     return (
         <Layout>
             <div className="page-container">
                 <AboutLanding />
-                {/* <MyHeader title="Background" id="background-header" /> */}
-                {/* <Timeline /> */}
+                <MyHeader title="Background" id="background-header" />
+                <Timeline />
             </div>
         </Layout>
     );
--- portfolio/src/components/Home/Timeline.jsx
+++ portfolio/src/components/Home/Timeline.jsx
@@ -7,10 +7,9 @@ export default function Timeline(){
                 <img src={sampadaPhotoPaths[1].info} alt={sampadaPhotoPaths[1].id} />
             </div>
             <div className="timeline-information">
-                
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid cumque id omnis molestiae quia! Cupiditate voluptatem repellendus iusto illum. Adipisci quisquam soluta dolorum nulla hic. Repellat eum veritatis voluptatibus nam.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, aut ad deleniti officia saepe commodi hic doloremque perspiciatis qui modi inventore excepturi voluptas non reiciendis obcaecati debitis? Obcaecati, sequi et!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit officia beatae corrupti dolorem voluptatum dignissimos totam voluptates repellat facere unde animi accusamus consectetur qui suscipit fuga vero aperiam, eos fugit.</p>
+                <p>I obtained my PhD in Mechanical Engineering from Carnegie Mellon University, where I was advised by Prof. Carmel Majidi. My doctoral research focused on nature-inspired micropatterned surfaces to better understand and control adhesion and friction, with applications in soft robotic locomotion and particulate grasping.</p>
+                <p>I completed my Master’s in Mechanical Engineering at CMU, working on liquid metal–based composites of self-healing soft polymers for smart orthotics, also advised by Prof. Carmel Majidi. My research journey began during my undergraduate internship at the Veermata Jijabai Technological Institute (University of Mumbai), where I worked in the Center of Excellence in Complex and Nonlinear Dynamical Systems under the guidance of Prof. Faruk Kazi where I worked on developing deperming protocols for marine vessels using cage systems and synthesis and characterization of magnetorheological fluids for shock absorption applications.</p>
+                <p>I am currently a postdoctoral research associate in Prof. Burak Ozdoganlar’s  Multiscale Manufacturing and Dynamics Laboratory at CMU, developing implantable, cell-based bioelectronic systems as part of the ARPA-H funded BIO-INSYNC consortium. Beyond the lab, I’m deeply passionate about mentorship and community building in STEM, and about creating spaces that foster collaboration, equity, and scientific growth.</p>
             </div>
         </div>
     );
